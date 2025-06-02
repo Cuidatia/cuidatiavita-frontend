4ebb0be217dfde46761098ade76902e681f173e4
MultiZapdos <dmocinha@alumnos.unex.es>
2025-06-02 14:14:49 +0200

    Clarity 2
    Config Clarity 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a] = c[a] || function() {
+                  (c[a].q = c[a].q || []).push(arguments);
+                };
+                t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+                y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+              })(window, document, "clarity", "script", "rt6kuejhx5");
+            `
+          }}
+        />
+      </head>
       <body>
         {children}
       </body>
