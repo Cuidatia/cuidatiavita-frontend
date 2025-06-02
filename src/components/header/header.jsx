d436b9e6186b324385965fe9eb8faa3f3c078306
MultiZapdos <dmocinha@alumnos.unex.es>
2025-06-02 14:03:17 +0200

    Clarity
    Clarity instalado

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,6 +4,13 @@ import logoCuidatiaVita from '../../../public/static/logoCuidatiaVita.png'
 export default function Header() {
     return(
         <header className='flex flex-wrap justify-between px-5 h-16 min-h-16 items-center text-center'>
+            <script type="text/javascript">
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "rt6kuejhx5");
+            </script>
             <div className='w-32 mx-4 inline-flex justify-center'>
                 <img src={logoCuidatiaVita.src} alt="logo" className='w-24 h-auto' />
             </div>
