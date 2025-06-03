b90c29ab3d8bcc8bbb790bbfb009da66fbb2d619
MultiZapdos <dmocinha@alumnos.unex.es>
2025-06-03 09:20:44 +0200

    Prueba Clarity 2
    Probando clarity

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
 import { SessionProvider } from 'next-auth/react';
+import { clarity } from 'clarity-js';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
+    useEffect(() => {
+        if (process.env.NODE_ENV === 'production') {
+            clarity.init("rt6kuejhx5");
+        }
+    }, []);
+
     return (
         <SessionProvider session={session}>
             <Component {...pageProps} />
         </SessionProvider>
     );
 }
+
