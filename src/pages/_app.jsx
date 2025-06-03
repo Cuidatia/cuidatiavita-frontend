5307aa62b080112f0c12d96ee9d8c755bc03150b
MultiZapdos <dmocinha@alumnos.unex.es>
2025-06-03 09:38:36 +0200

    C
    C

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,18 +1,24 @@
 import { useEffect } from 'react';
 import { SessionProvider } from 'next-auth/react';
-import Clarity from '@microsoft/clarity';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
-    useEffect(() => {
-        if (process.env.NODE_ENV === 'production') {
-            Clarity.init("rt6kuejhx5");
-        }
-    }, []);
+  useEffect(() => {
+    if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
+      import('@microsoft/clarity')
+        .then((clarity) => {
+          clarity.default.start('rt6kuejhx5');
+        })
+        .catch((err) => {
+          console.error('Error cargando Microsoft Clarity:', err);
+        });
+    }
+  }, []);
 
-    return (
-        <SessionProvider session={session}>
-            <Component {...pageProps} />
-        </SessionProvider>
-    );
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
+
 
