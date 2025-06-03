417af86436103431dcaae90ad113c50b0ddc8060
MultiZapdos <dmocinha@alumnos.unex.es>
2025-06-03 09:28:58 +0200

    Clar
    C

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,10 @@
 import { useEffect } from 'react';
 import { SessionProvider } from 'next-auth/react';
-import { clarity } from 'clarity-js';
 import Clarity from '@microsoft/clarity';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
     useEffect(() => {
         if (process.env.NODE_ENV === 'production') {
-            clarity.init("rt6kuejhx5");
             Clarity.init("rt6kuejhx5");
         }
     }, []);
