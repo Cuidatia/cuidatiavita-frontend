1b0eb688e1e7313044cdef9ad4bd40ffdd3e4e4a
MultiZapdos <dmocinha@alumnos.unex.es>
2025-06-03 10:20:14 +0200

    Clarity CSS
    Disculpadme por dejar los commits tan sucios

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { SessionProvider } from 'next-auth/react';
-import 'app/globals.css';
+import '../app/globals.css';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
     useEffect(() => {
