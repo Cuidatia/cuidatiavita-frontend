2d2fb41a054ea30af0def2ace6ea87a818f6f208
MultiZapdos <dmocinha@alumnos.unex.es>
2025-06-03 09:54:10 +0200

    C
    C

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,7 +4,7 @@ import { SessionProvider } from 'next-auth/react';
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
     useEffect(() => {
     console.log("NODE_ENV:", process.env.NODE_ENV);
-    if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
+    if (typeof window !== 'undefined') {
         console.log("Inicializando Clarity...");
         import('@microsoft/clarity')
         .then((clarity) => {
