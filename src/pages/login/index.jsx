eba5b63ac7dfd3b86deb2e97a55cbe770affeef2
MultiZapdos <dmocinha@alumnos.unex.es>
2025-10-27 12:17:33 +0100

    Footer 3
    3

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -69,9 +69,6 @@ export default function Login(){
                     </div>
                     <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Iniciar sesión</button>
                 </form>
-                <footer className="bg-white border-t border-gray-200 text-center py-4 text-sm text-gray-500">
-                    <Footer />
-                </footer>
                 {
                     message &&
                     <Alerts alertContent={message} alertType={'success'} />
--- src/pages/login/layout.jsx
+++ src/pages/login/layout.jsx
@@ -1,11 +1,15 @@
 import './styles.css';
-
+import Footer from "@/components/footer/footer";
 
 export default function LogInLayout({ children }) {
     return (
         <section className="bg-gray-50 body">
             <div className="h-screen grid place-content-center">
                 {children}
+                
+            </div>
+            <div className="bg-white border-t border-gray-200 text-center py-4 text-sm text-gray-500">
+                <Footer />
             </div>
         </section>
     );
